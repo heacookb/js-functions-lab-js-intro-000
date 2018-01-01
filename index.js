28b732417ce8b063c41ae3337eb76e5dbffdf75d
Brian Heacook <heacookb@protonmail.com>
2018-01-01 20:52:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function happyHolidayTo(holiday, name)
 }
 function holidayCountown(holiday, days)
 {
-  return("It's "+ (days) + "days until" + (holiday) + "")
+  return("It's " + (days) + "days until" + (holiday) + "")
 }
 
